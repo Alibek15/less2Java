aedc034a340e9fcb0e6e1a6639ae1638b46ac8d2
Alibek <52044412+Alibek15@users.noreply.github.com>
2022-08-26 17:05:55 +0600

    Add files via upload

---
+++ lesson2/src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) { //psvm + tab // точка входа
+        //System.out.println("Hello world"); //sout // вывод в консоль
+        Scanner scanner = new Scanner(System.in); // ввод в консоль
+
+        //next(), nextLine() - это работа с текстом
+//        System.out.println( "Введите имя");
+//
+//        String name = scanner.nextLine(); //ctr + alt + v
+//
+//        System.out.println("привет " + name);
+//
+//        System.out.println("Сколько лет?");
+//
+//        int age = scanner.nextInt();
+//
+//        System.out.printf("Вам %d лет", age);
+
+        //        && - лошическая и
+        //        || логическая или
+        //
+//        int day = scanner.nextInt();
+//        String month = scanner.next();
+//        int year = scanner.nextInt();
+//        System.out.printf("Сегодня %d день, %s месяц и %d год", day, month, year);
+//        int a=10;
+//
+//        int b=29;
+//
+//        int c= Math.pow(a,2)+...
+        var a=10;
+
+//        if (a>11){
+//            System.out.println("Больше");
+//        }else if(a>20){
+//
+//        }else
+
+//        switch(a){
+//            case 1:
+//                break;
+//        }\
+//        do{
+//
+//        }while{
+//
+//        }
+//        for (int i = 0; i < ; i++) {   //fori case
+//
+//        }
+        int[] booksIndex = new int[10]; // 1 variance
+
+        int[] number = {1,2,3,4}; // 2 variance
+
+        //ограничение по обьему
+        // нет расширения массива
+        //class arrays
+
+
+
+
+    }
+}
